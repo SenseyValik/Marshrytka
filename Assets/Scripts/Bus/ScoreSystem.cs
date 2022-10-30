a3d115a4cad8eed304188068e1e1d74a555c1deb
Vaidual <evgeniiycherepko@gmail.com>
2022-10-30 13:29:54 +0200

    wheel v3

--- Assets/Scripts/Bus/ScoreSystem.cs
+++ Assets/Scripts/Bus/ScoreSystem.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
     public int currentScore;
 
-    public float position;
+    public float lastPosition;
+
+    public Text scoreIndicatorText;
     void Start()
     {
         currentScore = 0;
-        position = transform.position.z;
+        lastPosition = transform.position.z;
+
+        scoreIndicatorText.text = "Score: " + currentScore;
     }
 
     // Update is called once per frame
@@ -21,6 +26,11 @@ public class ScoreSystem : MonoBehaviour
 
     void UpdateScore()
     {
-        int difference = (int)(transform.position.z - position);
+        int difference = (int)(transform.position.z - lastPosition);
+        if (difference > 0)
+        {
+            currentScore += difference;
+            scoreIndicatorText.text = "Score: " + currentScore;
+        }
     }
 }
